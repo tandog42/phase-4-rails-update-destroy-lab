38231f343241026df1a6358df5e74dbba6f36c4c
Tanner <troxs690@gmail.com>
2023-02-09 09:53:40 -0600

    D

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,6 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :cant_find
+
 
   # GET /plants
   def index
@@ -8,7 +10,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +20,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def cant_find
+    render json: {error: "UHH OHH no can do!"},status: :not_found
   end
 end
